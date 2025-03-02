5e98c593c6a34f19959776a7271f4523e40a9af8
khawkriab <khawkraib.dev@outlook.com>
2025-03-02 14:02:19 +0700

    test: deploy 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { GoogleSheetData } from "./components/GoogleSheets";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <GoogleSheetData />;
+  return (
+    <BrowserRouter basename="/my-app">
+      <GoogleSheetData />;
+    </BrowserRouter>
+  );
 }
 
 export default App;
